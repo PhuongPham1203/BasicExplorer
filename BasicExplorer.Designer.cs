2c06a74627a225c35990dd6564a6f85403329e3f
PhuongPham1203 <Phamphuong120398@gmail.com>
2022-02-13 19:26:15 +0700

    LastUpdate

--- BasicExplorer.Designer.cs
+++ BasicExplorer.Designer.cs
@@ -28,57 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("Node6");
-            System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("Node7");
-            System.Windows.Forms.TreeNode treeNode3 = new System.Windows.Forms.TreeNode("Node3", new System.Windows.Forms.TreeNode[] {
-            treeNode1,
-            treeNode2});
-            System.Windows.Forms.TreeNode treeNode4 = new System.Windows.Forms.TreeNode("Node4");
-            System.Windows.Forms.TreeNode treeNode5 = new System.Windows.Forms.TreeNode("Node5");
-            System.Windows.Forms.TreeNode treeNode6 = new System.Windows.Forms.TreeNode("Node0", new System.Windows.Forms.TreeNode[] {
-            treeNode3,
-            treeNode4,
-            treeNode5});
-            System.Windows.Forms.TreeNode treeNode7 = new System.Windows.Forms.TreeNode("Node1");
-            System.Windows.Forms.TreeNode treeNode8 = new System.Windows.Forms.TreeNode("Node8");
-            System.Windows.Forms.TreeNode treeNode9 = new System.Windows.Forms.TreeNode("Node9");
-            System.Windows.Forms.TreeNode treeNode10 = new System.Windows.Forms.TreeNode("Node2", new System.Windows.Forms.TreeNode[] {
-            treeNode8,
-            treeNode9});
-            System.Windows.Forms.TreeNode treeNode11 = new System.Windows.Forms.TreeNode("Node10");
-            System.Windows.Forms.TreeNode treeNode12 = new System.Windows.Forms.TreeNode("Node11");
-            System.Windows.Forms.TreeNode treeNode13 = new System.Windows.Forms.TreeNode("Node12");
-            System.Windows.Forms.TreeNode treeNode14 = new System.Windows.Forms.TreeNode("Node13");
-            System.Windows.Forms.TreeNode treeNode15 = new System.Windows.Forms.TreeNode("Node14");
-            System.Windows.Forms.TreeNode treeNode16 = new System.Windows.Forms.TreeNode("Node15");
-            System.Windows.Forms.TreeNode treeNode17 = new System.Windows.Forms.TreeNode("Node16");
-            System.Windows.Forms.TreeNode treeNode18 = new System.Windows.Forms.TreeNode("Node17");
-            System.Windows.Forms.TreeNode treeNode19 = new System.Windows.Forms.TreeNode("Node18");
-            System.Windows.Forms.TreeNode treeNode20 = new System.Windows.Forms.TreeNode("Node19");
-            System.Windows.Forms.TreeNode treeNode21 = new System.Windows.Forms.TreeNode("Node20");
-            System.Windows.Forms.TreeNode treeNode22 = new System.Windows.Forms.TreeNode("Node21");
-            System.Windows.Forms.TreeNode treeNode23 = new System.Windows.Forms.TreeNode("Node22");
-            System.Windows.Forms.TreeNode treeNode24 = new System.Windows.Forms.TreeNode("Node23");
-            System.Windows.Forms.TreeNode treeNode25 = new System.Windows.Forms.TreeNode("Node24");
-            System.Windows.Forms.TreeNode treeNode26 = new System.Windows.Forms.TreeNode("Node25");
-            System.Windows.Forms.TreeNode treeNode27 = new System.Windows.Forms.TreeNode("Node26");
-            System.Windows.Forms.TreeNode treeNode28 = new System.Windows.Forms.TreeNode("Node27");
-            System.Windows.Forms.ListViewItem listViewItem1 = new System.Windows.Forms.ListViewItem("File 1");
-            System.Windows.Forms.ListViewItem listViewItem2 = new System.Windows.Forms.ListViewItem("file 2");
-            System.Windows.Forms.ListViewItem listViewItem3 = new System.Windows.Forms.ListViewItem("file 3");
-            System.Windows.Forms.ListViewItem listViewItem4 = new System.Windows.Forms.ListViewItem("file 3");
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.panel1 = new System.Windows.Forms.Panel();
             this.treeViewRootFolder = new System.Windows.Forms.TreeView();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.listView1 = new System.Windows.Forms.ListView();
+            this.listViewFiles = new System.Windows.Forms.ListView();
             this.nameFile = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.sizeFile = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.dateModified = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.timeModified = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.labelFolder = new System.Windows.Forms.Label();
+            this.imageListDisk_Folder = new System.Windows.Forms.ImageList(this.components);
             this.menuStrip1.SuspendLayout();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
@@ -117,135 +81,61 @@
             // 
             // treeViewRootFolder
             // 
+            this.treeViewRootFolder.ImageIndex = 1;
+            this.treeViewRootFolder.ImageList = this.imageListDisk_Folder;
             this.treeViewRootFolder.Location = new System.Drawing.Point(3, 3);
             this.treeViewRootFolder.Name = "treeViewRootFolder";
-            treeNode1.Name = "Node6";
-            treeNode1.Text = "Node6";
-            treeNode2.Name = "Node7";
-            treeNode2.Text = "Node7";
-            treeNode3.Name = "Node3";
-            treeNode3.Text = "Node3";
-            treeNode4.Name = "Node4";
-            treeNode4.Text = "Node4";
-            treeNode5.Name = "Node5";
-            treeNode5.Text = "Node5";
-            treeNode6.Name = "Node0";
-            treeNode6.Text = "Node0";
-            treeNode7.Name = "Node1";
-            treeNode7.Text = "Node1";
-            treeNode8.Name = "Node8";
-            treeNode8.Text = "Node8";
-            treeNode9.Name = "Node9";
-            treeNode9.Text = "Node9";
-            treeNode10.Name = "Node2";
-            treeNode10.Text = "Node2";
-            treeNode11.Name = "Node10";
-            treeNode11.Text = "Node10";
-            treeNode12.Name = "Node11";
-            treeNode12.Text = "Node11";
-            treeNode13.Name = "Node12";
-            treeNode13.Text = "Node12";
-            treeNode14.Name = "Node13";
-            treeNode14.Text = "Node13";
-            treeNode15.Name = "Node14";
-            treeNode15.Text = "Node14";
-            treeNode16.Name = "Node15";
-            treeNode16.Text = "Node15";
-            treeNode17.Name = "Node16";
-            treeNode17.Text = "Node16";
-            treeNode18.Name = "Node17";
-            treeNode18.Text = "Node17";
-            treeNode19.Name = "Node18";
-            treeNode19.Text = "Node18";
-            treeNode20.Name = "Node19";
-            treeNode20.Text = "Node19";
-            treeNode21.Name = "Node20";
-            treeNode21.Text = "Node20";
-            treeNode22.Name = "Node21";
-            treeNode22.Text = "Node21";
-            treeNode23.Name = "Node22";
-            treeNode23.Text = "Node22";
-            treeNode24.Name = "Node23";
-            treeNode24.Text = "Node23";
-            treeNode25.Name = "Node24";
-            treeNode25.Text = "Node24";
-            treeNode26.Name = "Node25";
-            treeNode26.Text = "Node25";
-            treeNode27.Name = "Node26";
-            treeNode27.Text = "Node26";
-            treeNode28.Name = "Node27";
-            treeNode28.Text = "Node27";
-            this.treeViewRootFolder.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
-            treeNode6,
-            treeNode7,
-            treeNode10,
-            treeNode11,
-            treeNode12,
-            treeNode13,
-            treeNode14,
-            treeNode15,
-            treeNode16,
-            treeNode17,
-            treeNode18,
-            treeNode19,
-            treeNode20,
-            treeNode21,
-            treeNode22,
-            treeNode23,
-            treeNode24,
-            treeNode25,
-            treeNode26,
-            treeNode27,
-            treeNode28});
+            this.treeViewRootFolder.SelectedImageIndex = 0;
+            this.treeViewRootFolder.ShowPlusMinus = false;
             this.treeViewRootFolder.Size = new System.Drawing.Size(205, 405);
             this.treeViewRootFolder.TabIndex = 0;
+            this.treeViewRootFolder.AfterExpand += new System.Windows.Forms.TreeViewEventHandler(this.ShowSubFolder);
+            this.treeViewRootFolder.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.ShowFilesInFolder);
             // 
             // panel2
             // 
             this.panel2.AutoSize = true;
-            this.panel2.Controls.Add(this.listView1);
+            this.panel2.Controls.Add(this.listViewFiles);
             this.panel2.Location = new System.Drawing.Point(226, 27);
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(569, 411);
             this.panel2.TabIndex = 2;
             // 
-            // listView1
+            // listViewFiles
             // 
-            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.listViewFiles.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.nameFile,
             this.sizeFile,
             this.dateModified,
             this.timeModified});
-            this.listView1.HideSelection = false;
-            this.listView1.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
-            listViewItem1,
-            listViewItem2,
-            listViewItem3,
-            listViewItem4});
-            this.listView1.LabelEdit = true;
-            this.listView1.Location = new System.Drawing.Point(7, 3);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(559, 405);
-            this.listView1.TabIndex = 0;
-            this.listView1.UseCompatibleStateImageBehavior = false;
-            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listViewFiles.HideSelection = false;
+            this.listViewFiles.LabelEdit = true;
+            this.listViewFiles.Location = new System.Drawing.Point(7, 3);
+            this.listViewFiles.Name = "listViewFiles";
+            this.listViewFiles.Size = new System.Drawing.Size(559, 405);
+            this.listViewFiles.TabIndex = 0;
+            this.listViewFiles.UseCompatibleStateImageBehavior = false;
+            this.listViewFiles.View = System.Windows.Forms.View.Details;
             // 
             // nameFile
             // 
             this.nameFile.Text = "Name";
-            this.nameFile.Width = 91;
+            this.nameFile.Width = 297;
             // 
             // sizeFile
             // 
             this.sizeFile.Text = "Size";
+            this.sizeFile.Width = 68;
             // 
             // dateModified
             // 
-            this.dateModified.Text = "Date";
+            this.dateModified.Text = "Date Modified";
+            this.dateModified.Width = 95;
             // 
             // timeModified
             // 
-            this.timeModified.Text = "Time";
+            this.timeModified.Text = "Time Modified";
+            this.timeModified.Width = 78;
             // 
             // labelFolder
             // 
@@ -255,6 +145,14 @@
             this.labelFolder.Size = new System.Drawing.Size(104, 13);
             this.labelFolder.TabIndex = 5;
             this.labelFolder.Text = "3 Folder(s)    0 File(s)";
+            // 
+            // imageListDisk_Folder
+            // 
+            this.imageListDisk_Folder.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageListDisk_Folder.ImageStream")));
+            this.imageListDisk_Folder.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageListDisk_Folder.Images.SetKeyName(0, "icon_disk.png");
+            this.imageListDisk_Folder.Images.SetKeyName(1, "icon_folder.png");
+            this.imageListDisk_Folder.Images.SetKeyName(2, "icon_folder_open.png");
             // 
             // Form1
             // 
@@ -287,12 +185,13 @@
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.TreeView treeViewRootFolder;
-        protected internal System.Windows.Forms.ListView listView1;
+        protected internal System.Windows.Forms.ListView listViewFiles;
         private System.Windows.Forms.ColumnHeader nameFile;
         private System.Windows.Forms.ColumnHeader sizeFile;
         private System.Windows.Forms.ColumnHeader dateModified;
         private System.Windows.Forms.ColumnHeader timeModified;
         private System.Windows.Forms.Label labelFolder;
+        private System.Windows.Forms.ImageList imageListDisk_Folder;
     }
 }
 
--- BasicExplorer.cs
+++ BasicExplorer.cs
@@ -13,37 +13,208 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
+        //Image imageDisk ;
+        //Image imageFolder;
+
         public Form1()
         {
             InitializeComponent();
 
+            //this.imageDisk = new Bitmap(Application.StartupPath + "\\ico\\icon_disk.png");
+            //this.imageFolder = new Bitmap(Application.StartupPath + "\\ico\\icon_folder.png");
+
             LoadExplorer();
+
+            
         }
 
-        void LoadExplorer()
+        /*
+        void AddFolderFull(TreeNode treeNode) // Load full folder in 1 time
+        {
+            try
+            {
+
+                var listFolder = new DirectoryInfo(treeNode.Text).GetDirectories();
+                if (listFolder.Count() == 0)
+                {
+                    return;
+                }
+
+                foreach (DirectoryInfo folder in listFolder)
+                {
+                    if (Directory.Exists(folder.FullName))
+                    {
+                        TreeNode node = new TreeNode() { Text = folder.FullName };
+                        treeNode.Nodes.Add(node);
+                        AddFolderFull(node);
+                    }
+                }
+            }catch (Exception)
+            {
+                return ;
+            }
+        }
+        */
+
+        void LoadExplorer() // load Disk
         {
             int startDirNumber = 65;
 
-            for(int i = 0; i < 26; i++) // A=65, Z=90
+            for (int i = 0; i < 26; i++) // A=65, Z=90
             {
-                string stringPath = @"" + ((char)(startDirNumber+i)).ToString() + ":\\";
-                
+                string stringPath = @"" + ((char)(startDirNumber + i)).ToString() + ":\\";
+
 
                 if (Directory.Exists(stringPath))
                 {
-                    //MessageBox.Show(stringPath);
-                    TreeNode treeNode = new TreeNode()
-                    {
-                        Text = stringPath
-                    };
-
+                    TreeNode treeNode = new TreeNode() { Text = stringPath };
+                    treeNode.ImageIndex = 0;
+                    treeNode.SelectedImageIndex = 0;
                     treeViewRootFolder.Nodes.Add(treeNode);
+                    int deep = 2;
+                    AddFolder(treeNode,deep);
+
                 }
             }
 
-            
 
         }
 
+
+        void AddFolder(TreeNode treeNode, int deep) 
+        {
+            deep--;
+            if(deep <= 0) {
+                return; 
+            }
+
+            try
+            {
+                //MessageBox.Show(treeNode.FullPath);
+
+                var listFolder = new DirectoryInfo(treeNode.FullPath).GetDirectories();
+                if (listFolder.Count() == 0)
+                {
+                    return;
+                }
+                
+
+                foreach (DirectoryInfo folder in listFolder)
+                {
+                    if (Directory.Exists(folder.FullName))
+                    {
+                        TreeNode node = new TreeNode() { Text = folder.Name };
+                        
+                        node.ImageIndex = 1;
+                        node.SelectedImageIndex = 2;
+                        treeNode.Nodes.Add(node);
+                        AddFolder(node, deep);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return;
+            }
+        }
+
+        void UpdateFolderSelect(TreeNode treeNode)
+        {
+            try
+            {
+
+
+                var listFolder = new DirectoryInfo(treeNode.Text).GetDirectories();
+
+                //MessageBox.Show(treeNode.FullPath);
+
+                if (listFolder.Count() == 0)
+                {
+                    return;
+                }
+
+                treeNode.Nodes.Clear();
+
+                foreach (DirectoryInfo folder in listFolder)
+                {
+                    if (Directory.Exists(folder.FullName))
+                    {
+                        TreeNode node = new TreeNode() { Text = folder.Name };
+                        node.ImageIndex= 1;
+                        node.SelectedImageIndex = 2;
+                        treeNode.Nodes.Add(node);
+                        int deep = 3;
+                        AddFolder(node,deep);
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return;
+            }
+        }
+
+        
+
+        private void ShowSubFolder(object sender, TreeViewEventArgs e)
+        {
+            UpdateFolderSelect(treeViewRootFolder.SelectedNode);
+        }
+
+       
+
+        private void ShowFilesInFolder(object sender, TreeViewEventArgs e)
+        {
+            listViewFiles.Items.Clear();
+
+            DirectoryInfo d = new DirectoryInfo(treeViewRootFolder.SelectedNode.FullPath);
+            //MessageBox.Show(treeViewRootFolder.SelectedNode.FullPath);
+
+            FileInfo[] Files = d.GetFiles("*.*"); //Getting files
+            foreach (FileInfo file in Files)
+            {
+                ListViewItem item = new ListViewItem();
+                item.Text = file.Name;
+                item.SubItems.Add(new ListViewItem.ListViewSubItem() { Text = file.Length.ToString(), });
+                item.SubItems.Add(new ListViewItem.ListViewSubItem() { Text = file.LastWriteTime.ToString("yyyy/MM/dd"), });
+                item.SubItems.Add(new ListViewItem.ListViewSubItem() { Text = file.LastWriteTime.ToString("HH:mm"), });
+
+                listViewFiles.Items.Add(item);
+            }
+
+            // Count Folder and Files
+            var listFolder = d.GetDirectories();
+            int numberOfFolder = listFolder.Length;
+            int numberOfFile = Files.Length;
+
+            labelFolder.Text = numberOfFolder.ToString()+" Folder(s)   "+numberOfFile.ToString()+" File(s)";
+
+
+        }
+
+        
+        /*
+private void ShowFilesInFolder(object sender, TreeNodeMouseClickEventArgs e)
+{
+listViewFiles.Items.Clear();
+
+DirectoryInfo d = new DirectoryInfo(treeViewRootFolder.SelectedNode.FullPath);
+MessageBox.Show(treeViewRootFolder.SelectedNode.FullPath);
+
+FileInfo[] Files = d.GetFiles("*.*"); //Getting files
+foreach (FileInfo file in Files)
+{
+ListViewItem item = new ListViewItem();
+item.Text = file.Name;
+item.SubItems.Add(new ListViewItem.ListViewSubItem() { Text = file.Length.ToString(), });
+item.SubItems.Add(new ListViewItem.ListViewSubItem() { Text = file.LastWriteTime.ToString("yyyy/MM/dd"), });
+item.SubItems.Add(new ListViewItem.ListViewSubItem() { Text = file.LastWriteTime.ToString("HH:mm"), });
+
+listViewFiles.Items.Add(item);
+}
+
+}
+*/
     }
 }
